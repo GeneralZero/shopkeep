398338ecd9b8415860927ca18b9b0ba9a73ece3a
Tyler Anthony Romeo <tylerromeo@gmail.com>
2013-01-26 16:46:44 -0500

    Added model for Collections (collection/wishlist/shop).
    Created model class for collections/wishlists/shops. Needs
    the Item model in order to work with search queries.

---
+++ models/Collection.py
@@ -0,0 +1,21 @@
+from google.appengine.ext import db
+from google.appengine.api import users
+from . import Item
+
+TYPE_WISHLIST = 0
+TYPE_SHOP = 1
+TYPE_COLLECTION = 2
+
+class Collection(db.model):
+	"""Represents a private or public collection of items."""
+	type = db.IntegerProperty(required=True, choices=set([TYPE_WISHLIST, TYPE_SHOP, TYPE_COLLECTION])
+	owners = db.ListProperty(users.User, required=True)
+	items = db.ListProperty(db.Key)
+	query = db.ListProperty(str)
+
+	@classmethod
+	def getItems(cls):
+		if query:
+			return Item.search(self.query)
+		else:
+			return Item.get(self.items)
